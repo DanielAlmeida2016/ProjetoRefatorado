1f238cc7dbb3d4379a34402a4a651ea32a0d82d5
PaamFidelis <fidelis.pamella@gmail.com>
2017-05-03 22:13:12 -0300

    Add files via upload
    Remoção do atributo "fornecedor"

--- WEB-INF/classes/projeto/refatorado/test/ProdutoTest.java
+++ WEB-INF/classes/projeto/refatorado/test/ProdutoTest.java
@@ -9,91 +9,86 @@ import projeto.refatorado.model.Produto;
 import projeto.refatorado.service.ProdutoService;
 
 public class ProdutoTest {
-	
+
 	Produto p, copia;
 	ProdutoService ps;
 	long id;
-	
 
 	@Before
 	public void setUp() throws Exception {
-		
+
 		ps = new ProdutoService();
-		p = new Produto();	
+		p = new Produto();
 		p.setCategoria("Roupa");
 		p.setDescricao("Calça");
-		p.setFornecedor("Hering");
 		p.setQuantidade(10);
 		p.setValorCompra(50.00);
 		p.setValorVenda(100.00);
-		
+
 		copia = p;
-		
-		//System.out.println("setup");
+
+		// System.out.println("setup");
 	}
 
 	@Test
 	public void test001Criar() {
-		
+
 		System.out.println("criar");
-		
+
 		System.out.println(p.toString());
-		
+
 		ps.criar(p);
-		
-		//id = p.getId();
-				
-		//System.out.println(p.getId());
-		
+
+		// id = p.getId();
+
+		// System.out.println(p.getId());
+
 		assertEquals("Criar Teste", p, copia);
-		
-	}	
-	
+
+	}
+
 	@Test
-	public void test002Carregar(){	
+	public void test002Carregar() {
 		System.out.println("carregar");
 		ps.carregar(1);
-		
+
 		assertEquals("Carregar Teste", p, copia);
-		
+
 	}
-	
+
 	@Test
-	public void test003Alterar(){
-		
+	public void test003Alterar() {
+
 		System.out.println("Alterar");
-		
+
 		Produto produto = ps.carregar(2);
-		
-		produto.setFornecedor("Marisa");
-		copia.setFornecedor("Marisa");
+
+		produto.setDescricao("Marisa");
+		copia.setDescricao("Marisa");
 		copia.setId(2L);
-		
+
 		ps.alterar(produto);
-		
+
 		assertEquals(ps.carregar(2), copia);
 	}
-	
+
 	@Test
-	public void test004Excluir(){
-		
+	public void test004Excluir() {
+
 		System.out.println("Alterar");
-		
+
 		Produto produtoExclusao = ps.carregar(2);
-		
+
 		ps.excluir(produtoExclusao);
-		
+
 		copia.setCategoria(null);
 		copia.setDescricao(null);
-		copia.setFornecedor(null);
 		copia.setQuantidade(0);
 		copia.setValorCompra(0.0);
 		copia.setValorVenda(0.0);
-		
+
 		assertEquals(p, copia);
-		
-		
-		
+
 	}
 
 }
